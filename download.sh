dc87708488ff9bc7ab0807dd9aac477c042a23b2
Marc Partensky <marc.partensky@gmail.com>
2023-01-09 19:16:36 +0100

    download.sh that works

--- download.sh
+++ download.sh
@@ -2,6 +2,5 @@
 
 mkdir data
 wget https://zissou.infosci.cornell.edu/convokit/datasets/movie-corpus/movie-corpus.zip
-unzip movie-corpus.zip
+unzip movie-corpus.zip -d data
 rm movie-corpus.zip
-mv movie-corpus data
